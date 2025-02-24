42923be08dc5dbc056a5861c91daefffc65604a7
Gaurav Rajput <147949028+ErGauravRajput@users.noreply.github.com>
2025-02-24 20:34:07 +0530

    Update GetMoviesDetail.jsx

--- src/Api/GetMoviesDetail.jsx
+++ src/Api/GetMoviesDetail.jsx
@@ -1,7 +1,7 @@
 export const getMovieDetails = async ({ params }) => {
     const id = params.MoviesID;
     console.log(id);
-    const API = `http://www.omdbapi.com/?i=${id}&apikey=477f9334`;
+    const API = `https://www.omdbapi.com/?i=${id}&apikey=477f9334`;
     try {
         const res = await fetch(API);
         const data = await res.json();
